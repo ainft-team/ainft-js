88c93e3f923cad91e469e63cce4f2cc48a86f7ea
liayoo <lia@comcom.ai>
2022-09-21 16:52:19 +0900

    Add getNftContractBySymbol, getNft, update nft api queries.

--- src/asset.ts
+++ src/asset.ts
@@ -4,17 +4,72 @@ import { HttpMethod } from './types';
 import { buildData } from './util';
 
 export default class Asset extends AinftBase {
+  async getNftContractBySymbol(appId: string, symbol: string) {
+    const timestamp = Date.now();
+    const query = { appId, symbol: encodeURIComponent(symbol) };
+    const trailingUrl = 'nft';
+    const data = buildData(
+      HttpMethod.GET,
+      `/asset/${trailingUrl}`,
+      timestamp,
+      query
+    );
+    const signature = this.signData(data);
+    return axios
+      .get(`${this.baseUrl}/${trailingUrl}`, {
+        params: query,
+        headers: {
+          'X-AINFT-Date': timestamp,
+          Authorization: `AINFT ${signature}`,
+        },
+      })
+      .then((res) => res.data.data)
+      .catch((e) => {
+        throw e.response.data;
+      });
+  }
+
+  async getNft(
+    appId: string,
+    chainId: string,
+    contractAddress: string,
+    tokenId: string,
+  ) {
+    const timestamp = Date.now();
+    const query = { appId, contractAddress, tokenId };
+    const trailingUrl = `nft/${chainId}`;
+    const data = buildData(
+      HttpMethod.GET,
+      `/asset/${trailingUrl}`,
+      timestamp,
+      query
+    );
+    const signature = this.signData(data);
+    return axios
+      .get(`${this.baseUrl}/${trailingUrl}`, {
+        params: query,
+        headers: {
+          'X-AINFT-Date': timestamp,
+          Authorization: `AINFT ${signature}`,
+        },
+      })
+      .then((res) => res.data.data)
+      .catch((e) => {
+        throw e.response.data;
+      });
+  }
+
   async getUserNftList(
     appId: string,
     chainId: string,
     ethAddress: string,
-    contractAddress?: string
+    contractAddress: string,
+    tokenId?: string,
   ) {
     const timestamp = Date.now();
-    const query = { appId };
-    const trailingUrl = contractAddress
-      ? `nft/${chainId}/${ethAddress}/${contractAddress}`
-      : `nft/${chainId}/${ethAddress}`;
+    const query: any = { appId, contractAddress };
+    if (tokenId) query.tokenId = tokenId;
+    const trailingUrl = `nft/${chainId}/${ethAddress}`;
     const data = buildData(
       HttpMethod.GET,
       `/asset/${trailingUrl}`,
