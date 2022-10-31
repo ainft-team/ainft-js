b93e1623d647b43e9ee9b13924783fe6a0cc6772
hyeonwoong <hyeonwoong@comcom.ai>
2022-10-31 18:01:35 +0900

    feat: add registerKey method

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,12 @@ import { HttpMethod, User } from './types';
 
 export default class Auth extends AinftBase {
 
+  registerKey(appId: string, address: string) {
+    const body = { appId, address };
+    const trailingUrl = 'register_key';
+    return this.sendRequest(HttpMethod.POST, trailingUrl, body);
+  }
+
   getUser(appId: string, userId: string): Promise<User> {
     const query = { appId };
     const trailingUrl = `user/${userId}`;
