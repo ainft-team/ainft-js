2b7f8f8f7808f48dc5cfc32956869b7bc38c0021
hyeonwoong <hyeonwoong@comcom.ai>
2022-11-01 19:04:34 +0900

    feat: add auth methods

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,45 @@
 import AinftBase from './ainftBase';
+import { INITIALIZE_STAKE_AMOUNT } from './constants';
 import { HttpMethod, User } from './types';
 
 export default class Auth extends AinftBase {
 
-  registerKey(appId: string, address: string) {
-    const body = { appId, address };
-    const trailingUrl = 'register_key';
+  async initializeApp(appId: string, userId: string, accessKey: string): Promise<void> {
+    const createAppRes = await this.createApp(appId, userId, accessKey);
+    console.log(`create app tx hash - ${createAppRes.txHash}`);
+    const stakeRes = await this.stake(appId, userId, INITIALIZE_STAKE_AMOUNT);
+    console.log(`stake tx hash - ${stakeRes.txHash}`);
+    const setRuleRes = await this.setBlockchainActivityRule(appId);
+    console.log(`set rule tx hash - ${setRuleRes.txHash}`);
+  }
+
+  createApp(appId: string, userId: string, accessKey: string) {
+    const body = { appId, userId, accessKey };
+    const trailingUrl = 'create_app';
+    return this.sendRequest(HttpMethod.POST, trailingUrl, body);
+  }
+
+  stake(appId: string, userId: string, amount: number) {
+    const body = { appId, userId, amount };
+    const trailingUrl = 'stake';
+    return this.sendRequest(HttpMethod.POST, trailingUrl, body);
+  }
+
+  createUser(appId: string, userId: string): Promise<User> {
+    const body = { appId, userId };
+    const trailingUrl = 'create_user_account';
+    return this.sendRequest(HttpMethod.POST, trailingUrl, body);
+  }
+
+  createAdmin(appId: string, userId: string): Promise<User> {
+    const body = { appId, userId };
+    const trailingUrl = 'create_admin_account';
+    return this.sendRequest(HttpMethod.POST, trailingUrl, body);
+  }
+
+  setBlockchainActivityRule(appId: string) {
+    const body = { appId };
+    const trailingUrl = 'ain_rule/activity';
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
 
