c2ef005db456f7658fc5d7b04563dc4154c279a1
hyeonwoong <hyeonwoong@comcom.ai>
2022-11-07 15:43:19 +0900

    fix: comment in console log

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import AinftBase from './ainftBase';
 import { HttpMethod, User } from './types';
 export default class Auth extends AinftBase {
   async initializeApp(appId: string, userId: string): Promise<void> {
-    console.log('Start app initializing... This may take up to a minute.');
+    console.log('Starting app initialization... This may take up to a minute.');
     const accessKey = this.ain.wallet.defaultAccount?.address!;
     const createAppRes = await this.createApp(
       appId,
