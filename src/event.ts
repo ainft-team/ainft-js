c2fe50b8e7c897256ddcb737702b60c46e5e7fc1
hyeonwoong <hyeonwoong@comcom.ai>
2022-12-12 17:03:08 +0900

    docs: update event document

--- src/event.ts
+++ src/event.ts
@@ -16,7 +16,10 @@ import {
 } from './types';
 
 export default class Event extends AinftBase {
-
+  /**
+   * Create a new event. Set the tasks to be performed and the rewards to receive.
+   * @param {CreateEventParams} CreateEventParams
+   */
   create({
     appId,
     eventId,
@@ -41,6 +44,10 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
 
+  /**
+   * Updates an existing event.
+   * @param {Partial<CreateEventParams>} UpdateEventParams
+   */
   update({
     appId,
     eventId,
@@ -62,12 +69,22 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.PUT, trailingUrl, body);
   }
 
+  /**
+   * Delete an existing event.
+   * @param {string} appId
+   * @param {string} eventId
+   */
   delete(appId: string, eventId: string): Promise<void> {
     const query = { appId };
     const trailingUrl = `${eventId}`;
     return this.sendRequest(HttpMethod.DELETE, trailingUrl, query);
   }
 
+  /**
+   * Get event.
+   * @param {string} appId
+   * @param {string} eventId
+   */
   get(appId: string, eventId: string): Promise<TokenomicsEvent> {
     const query = { appId };
     const trailingUrl = `${eventId}`;
@@ -75,6 +92,11 @@ export default class Event extends AinftBase {
   }
 
   // NOTE(liayoo): calling this function will create a user if the userId doesn't exist.
+  /**
+   * Add an activity that matches the event.
+   * @param {AddEventActivityParams} AddEventActivityParams
+   * @returns return activity ID.
+   */
   addActivity({
     appId,
     userId,
@@ -94,6 +116,10 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
 
+  /**
+   * @param {GetEventActivityParams} GetEventActivityParams
+   * @returns {Promise<Activity | null>} Return activity object or null.
+   */
   getActivity({
     appId,
     userId,
@@ -113,6 +139,10 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.GET, trailingUrl, query);
   }
 
+  /**
+   * Update the activity's status. Activity status includes CREATED, REWARDED, and FAILED.
+   * @param {UpdateEventActivityStatusParams} UpdateEventActivityStatusParams
+   */
   updateActivityStatus({
     eventId,
     activityId,
@@ -129,24 +159,49 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.PUT, trailingUrl, body);
   }
 
+  /**
+   * Returns a list of TaskTypes to use for events.
+   * @param {string} appId
+   */
   getTaskTypeList(appId: string): Promise<TaskType[]> {
     const query = { appId };
     const trailingUrl = 'task-types';
     return this.sendRequest(HttpMethod.GET, trailingUrl, query);
   }
 
+  /**
+   * Returns a list of RewardTypes to use for events.
+   * @param {string} appId
+   */
   getRewardTypeList(appId: string): Promise<RewardType[]> {
     const query = { appId };
     const trailingUrl = 'reward-types';
     return this.sendRequest(HttpMethod.GET, trailingUrl, query);
   }
 
-  getPendingRewards(appId: string, userId: string, eventId: string): Promise<PendingRewards> {
+  /**
+   * Returns user's pending reward from event.
+   * @param {string} appId
+   * @param {string} userId
+   * @param {string} eventId
+   */
+  getPendingRewards(
+    appId: string,
+    userId: string,
+    eventId: string
+  ): Promise<PendingRewards> {
     const query = { appId, userId };
     const trailingUrl = `${eventId}/pending-rewards`;
     return this.sendRequest(HttpMethod.GET, trailingUrl, query);
   }
 
+  /**
+   * Provide event rewards to users.
+   * @param {string} appId
+   * @param {string} userId
+   * @param {string} eventId
+   * @param {RewardOptions} options If the reward has not been confirmed, you can enter the amount of reward.
+   */
   reward(
     appId: string,
     userId: string,
@@ -156,13 +211,23 @@ export default class Event extends AinftBase {
     const body: any = {
       appId,
       userId,
-      ...options && { options }
+      ...(options && { options }),
     };
     const trailingUrl = `${eventId}/reward`;
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
 
-  getRewardHistory(appId: string, userId: string, eventId: string): Promise<RewardInfo[]> {
+  /**
+   * Return list of user's reward history.
+   * @param {string} appId
+   * @param {string} userId
+   * @param {string} eventId
+   */
+  getRewardHistory(
+    appId: string,
+    userId: string,
+    eventId: string
+  ): Promise<RewardInfo[]> {
     const query = {
       appId,
       userId,
@@ -171,7 +236,17 @@ export default class Event extends AinftBase {
     return this.sendRequest(HttpMethod.GET, trailingUrl, query);
   }
 
-  getActivityHistory(appId: string, userId: string, eventId: string): Promise<Activity[]> {
+  /**
+   * Return list of user's activity.
+   * @param {string} appId
+   * @param {string} userId
+   * @param {string} eventId
+   */
+  getActivityHistory(
+    appId: string,
+    userId: string,
+    eventId: string
+  ): Promise<Activity[]> {
     const query = {
       appId,
       userId,
