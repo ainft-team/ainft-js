a40837ad34e949ee8838487e3bcd7771b2367db3
hyeonwoong <hyeonwoong@comcom.ai>
2023-01-20 10:30:23 +0900

    add docs

--- src/activity.ts
+++ src/activity.ts
@@ -2,12 +2,20 @@ import AinftBase from "./ainftBase";
 import { ActivityNftInfo, HttpMethod, NftActivityType, TaskTypeCategory } from "./types";
 
 export default class Activity extends AinftBase {
+  /**
+   * You can add activity without event. Just add activity.
+   * @param appId 
+   * @param userId 
+   * @param data 
+   * @param activityType 
+   * @param nftInfo 
+   */
   add(
     appId: string,
     userId: string,
     data: any,
     activityType: TaskTypeCategory | NftActivityType,
-    nftInfo?: ActivityNftInfo,
+    nftInfo?: ActivityNftInfo
   ) {
     const body = {
       appId,
@@ -20,6 +28,14 @@ export default class Activity extends AinftBase {
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
 
+  /**
+   * You can update the record of one activity with NFT. record can be a statistic or count value from activity.
+   * @param appId
+   * @param userId
+   * @param nftInfo
+   * @param label Record label. Record data is recorded under the label.
+   * @param data
+   */
   addNftRecord(
     appId: string,
     userId: string,
@@ -33,7 +49,7 @@ export default class Activity extends AinftBase {
       nftInfo,
       label,
       data,
-    }
+    };
     const trailingUrl = 'nft/record';
     return this.sendRequest(HttpMethod.POST, trailingUrl, body);
   }
