290875b9c793737bf540f0df18c627d419b8b436
ehgmsdk20 <kdh75576795@gmail.com>
2023-02-07 14:29:07 +0900

    Add setNftMetadata

--- test/status.test.ts
+++ test/status.test.ts
@@ -1,11 +1,13 @@
-import AinftJs from '../src/index';
+import AinftJs from '../src/ainft';
 
 describe("Status", () => {
   it("should return health", async () => {
     // TODO(hyeonwoong): remove dev api endpoint.
     const ainftJs = new AinftJs(
+      'a'.repeat(64),
       'https://ainft-api-dev.ainetwork.ai',
-      'a'.repeat(64), // TODO(lia): use an access key for testing
+      "https://testnet-api.ainetwork.ai", // TODO(lia): use an access key for testing,
+      0
     );
     expect(await ainftJs.getStatus()).toMatchObject({ health: true });
   });
