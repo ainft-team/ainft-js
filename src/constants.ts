c41d37a57ff63886b10a3b4bdd0bfec2a7cfb58c
hyeonwoong <hyeonwoong@comcom.ai>
2023-07-07 18:07:05 +0900

    fix: update ainftJs constructor

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,20 @@
 export const NODE_ENV = process.env.NODE_ENV;
-export const AINFT_SERVER_ENDPOINT = NODE_ENV === 'prod' ?
-  'https://ainft-api.ainetwork.ai':
-  'https://ainft-api-dev.ainetwork.ai';
 
-export const AIN_BLOCKCHAIN_ENDPOINT = NODE_ENV === 'prod'
-  ? 'https://mainnet-api.ainetwork.ai'
-  : 'https://testnet-api.aientwork.ai';
+export const AINFT_SERVER_ENDPOINT = {
+  dev: 'https://ainft-api-dev.ainetwork.ai',
+  prod: 'https://ainft-api.ainetwork.ai',
+}
 
-export const AIN_BLOCKCHAIN_CHAINID = NODE_ENV === 'prod' ? 1 : 0;
+export const AIN_BLOCKCHAIN_ENDPOINT = {
+  dev: 'https://testnet-api.ainetwork.ai',
+  prod: 'https://mainnet-api.ainetwork.ai',
+}
+
+
+export const AIN_BLOCKCHAIN_CHAINID = {
+  dev: 0,
+  prod: 1
+}
 
 export const MIN_GAS_PRICE = 500;
 export const APP_STAKING_LOCKUP_DURATION_MS = 30 * 1000 // 30 seconds
