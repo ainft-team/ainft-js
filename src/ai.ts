2eb8aa60b617f526103a2c4606f3e00abae1fdb5
jiyoung <jiyoungan@comcom.ai>
2023-12-08 08:03:14 +0900

    feat: add connect, disconnect methods.

--- src/ai.ts
+++ src/ai.ts
@@ -1,5 +1,9 @@
 import Ain from "@ainblockchain/ain-js";
 import Ainize from "@ainize-team/ainize-sdk";
+import Ainft721Object from "./ainft721Object";
+import { Path } from "./constants";
+import { AiConnectionStatus } from "./types";
+import { buildTransactionBody } from "./util";
 
 export default class AI {
   private ain: Ain;
@@ -10,8 +14,74 @@ export default class AI {
     this.ainize = ainize;
   }
 
-  connect(): void {}
-  disconnect(): void {}
+  async connect(
+    ainftObject: Ainft721Object,
+    serviceName: string
+  ): Promise<any> {
+    const appId = ainftObject.appId;
+    const appPath = Path.app(appId).root();
+    const app = await this.ain.db.ref(appPath).getValue();
+    if (!app) {
+      throw new Error("AINFT object not found");
+    }
+
+    const address = this.ain.signer.getAddress();
+    if (address !== app.owner) {
+      throw new Error(`Not AINFT object owner: ${address}`);
+    }
+
+    const service = await this.ainize!.getService(serviceName);
+    // TODO(jiyoung): update to handle boolean return from service.isRunning() when implemented.
+    await service.isRunning();
+
+    const txBody = buildTransactionBody({
+      type: "SET_VALUE",
+      ref: `/apps/${appId}/ai/${serviceName}`,
+      value: {
+        status: AiConnectionStatus.CONNECTED,
+        timestamp: Date.now(),
+      },
+    });
+    return this.ain.sendTransaction(txBody);
+  }
+
+  async disconnect(
+    ainftObject: Ainft721Object,
+    serviceName: string
+  ): Promise<any> {
+    const appId = ainftObject.appId;
+    const appPath = Path.app(appId).root();
+    const app = await this.ain.db.ref(appPath).getValue();
+    if (!app) {
+      throw new Error("AINFT object not found");
+    }
+
+    const address = this.ain.signer.getAddress();
+    if (address !== app.owner) {
+      throw new Error(`Not AINFT object owner: ${address}`);
+    }
+
+    const connectionStatusPath = Path.app(appId).connectionStatus(serviceName);
+    const connectionStatus = await this.ain.db
+      .ref(connectionStatusPath)
+      .getValue();
+    if (
+      !connectionStatus ||
+      connectionStatus !== AiConnectionStatus.CONNECTED
+    ) {
+      throw new Error(`AINFT object not connected to service`);
+    }
+
+    const txBody = buildTransactionBody({
+      type: "SET_VALUE",
+      ref: `/apps/${appId}/ai/${serviceName}`,
+      value: {
+        status: AiConnectionStatus.DISCONNECTED,
+        timestamp: Date.now(),
+      },
+    });
+    return this.ain.sendTransaction(txBody);
+  }
 
   getStatus(): void {}
 
--- src/constants.ts
+++ src/constants.ts
@@ -22,3 +22,12 @@ export const APP_STAKING_LOCKUP_DURATION_MS = 30 * 1000 // 30 seconds
 export const SUPPORTED_AINFT_STANDARDS = {
   721: '721',
 }
+
+export const Path = {
+  app: (appId: string): any => {
+    return {
+      root: () => `/apps/${appId}`,
+      connectionStatus: (serviceName: string) => `${Path.app(appId).root()}/ai/${serviceName}/status`
+    };
+  },
+}
