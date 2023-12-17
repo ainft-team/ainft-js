387c0612532f1279e45ef89696b80568dc17b6da
jiyoung <jiyoungan@comcom.ai>
2023-12-17 18:36:36 +0900

    feat: add SdkBase class.

--- src/ai/base.ts
+++ src/ai/base.ts
@@ -1,15 +1,6 @@
-import Ain from '@ainblockchain/ain-js';
-import Ainize from '@ainize-team/ainize-js';
+import SdkBase from '../sdkBase';
 import Chat from './chat';
 
-export default class BaseAi {
-  chat: Chat;
-  private ain: Ain;
-  private ainize: Ainize;
-
-  constructor(ain: Ain, ainize: Ainize) {
-    this.chat = new Chat(ain, ainize);
-    this.ain = ain;
-    this.ainize = ainize;
-  }
+export default class BaseAi extends SdkBase {
+  chat: Chat = new Chat(this.ain, this.ainize);
 }
