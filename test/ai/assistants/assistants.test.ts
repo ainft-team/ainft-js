7a7dfa61bd972115e9523a25ffc811f38138bb54
jiyoung <jiyoungan@comcom.ai>
2023-12-20 23:01:16 +0900

    test: add update assistant test.

--- test/ai/assistants/assistants.test.ts
+++ test/ai/assistants/assistants.test.ts
@@ -10,10 +10,11 @@ const objectId = '0x45E89F37Cee508cf0D4F6e74b35EfeBdd90BD731';
 const appId = 'ainft721_0x45e89f37cee508cf0d4f6e74b35efebdd90bd731';
 const serviceName = 'ainize_test14';
 const tokenId = '1';
+const assistantId = 'asst_000000000000000000000001';
 
 describe('Assistant', () => {
   it('create: should create assistant with required params', async () => {
-    const result = await ainft.ai.chat.assistants.create({
+    const txResult = await ainft.ai.chat.assistants.create({
       config: {
         provider: 'openai',
         api: 'assistants',
@@ -29,15 +30,8 @@ describe('Assistant', () => {
       .ref(`/apps/${appId}/tokens/${tokenId}/ai/${serviceName}`)
       .getValue();
 
-    expect(result.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
-    expect(result.result).toBeDefined();
-    expect(result.assistant.id).toMatch(/^asst_[A-Za-z0-9]{24}/);
-    expect(result.assistant.model).toBe('gpt-3.5-turbo');
-    expect(result.assistant.name).toBe('name');
-    expect(result.assistant.instructions).toBe('instructions');
-    expect(result.assistant.description).toBeNull();
-    expect(result.assistant.metadata).toEqual({});
-
+    expect(txResult.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
+    expect(txResult.result).toBeDefined();
     expect(value.id).toMatch(/^asst_[A-Za-z0-9]{24}/);
     expect(value.config.model).toBe('gpt-3.5-turbo');
     expect(value.config.name).toBe('name');
@@ -47,7 +41,7 @@ describe('Assistant', () => {
   });
 
   it('create: should create assistant with required and optional params', async () => {
-    const result = await ainft.ai.chat.assistants.create({
+    const txResult = await ainft.ai.chat.assistants.create({
       config: {
         provider: 'openai',
         api: 'assistants',
@@ -65,14 +59,8 @@ describe('Assistant', () => {
       .ref(`/apps/${appId}/tokens/${tokenId}/ai/${serviceName}`)
       .getValue();
 
-    expect(result.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
-    expect(result.result).toBeDefined();
-    expect(result.assistant.id).toMatch(/^asst_[A-Za-z0-9]{24}/);
-    expect(result.assistant.model).toBe('gpt-3.5-turbo');
-    expect(result.assistant.name).toBe('name');
-    expect(result.assistant.instructions).toBe('instructions');
-    expect(result.assistant.description).toBe('description');
-    expect(result.assistant.metadata).toEqual({ key: 'value' });
+    expect(txResult.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
+    expect(txResult.result).toBeDefined();
 
     expect(value.id).toMatch(/^asst_[A-Za-z0-9]{24}/);
     expect(value.config.model).toBe('gpt-3.5-turbo');
@@ -80,5 +68,68 @@ describe('Assistant', () => {
     expect(value.config.instructions).toBe('instructions');
     expect(value.config.description).toBe('description');
     expect(value.config.metadata).toEqual({ key: 'value' });
+  });
+
+  it('update: should not update assistant with required params', async () => {
+    const txResult = await ainft.ai.chat.assistants.update(assistantId, {
+      config: {
+        provider: 'openai',
+        api: 'assistants',
+        objectId: objectId,
+      },
+      tokenId: tokenId,
+    });
+
+    const value = await ainft.ain.db
+      .ref(`/apps/${appId}/tokens/${tokenId}/ai/${serviceName}`)
+      .getValue();
+
+    expect(txResult.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
+    expect(txResult.result).toBeDefined();
+    expect(value.id).toMatch(assistantId);
+    expect(value.config.model).toBe('gpt-3.5-turbo');
+    expect(value.config.name).toBe('name');
+    expect(value.config.instructions).toBe('instructions');
+    expect(value.config).not.toHaveProperty('description');
+    expect(value.config).not.toHaveProperty('metadata');
+  });
+
+  it('update: should update assistant with required and optional params', async () => {
+    const txResult = await ainft.ai.chat.assistants.update(assistantId, {
+      config: {
+        provider: 'openai',
+        api: 'assistants',
+        objectId: objectId,
+      },
+      model: 'gpt-4',
+      name: 'new_name',
+      instructions: 'new_instructions',
+      description: 'new_description',
+      metadata: {
+        key1: 'value1',
+        key2: 'value2',
+        key3: 'value3',
+        key4: 'value4',
+      },
+      tokenId: tokenId,
+    });
+
+    const value = await ainft.ain.db
+      .ref(`/apps/${appId}/tokens/${tokenId}/ai/${serviceName}`)
+      .getValue();
+
+    expect(txResult.tx_hash).toMatch(/^0x[a-fA-F0-9]{64}$/);
+    expect(txResult.result).toBeDefined();
+    expect(value.id).toMatch(assistantId);
+    expect(value.config.model).toBe('gpt-4');
+    expect(value.config.name).toBe('new_name');
+    expect(value.config.instructions).toBe('new_instructions');
+    expect(value.config.description).toBe('new_description');
+    expect(value.config.metadata).toEqual({
+      key1: 'value1',
+      key2: 'value2',
+      key3: 'value3',
+      key4: 'value4',
+    });
   });
 });
