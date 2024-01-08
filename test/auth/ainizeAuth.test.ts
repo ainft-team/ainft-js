22b9b84ae92b79a4753f732431943cb475424784
jiyoung <jiyoungan@comcom.ai>
2024-01-08 16:52:06 +0900

    test: add ainize authentication test.

---
+++ test/auth/ainizeAuth.test.ts
@@ -0,0 +1,32 @@
+import AinftJs from '../../src/ainft';
+import AinizeAuth from '../../src/auth/ainizeAuth';
+
+describe('Ainize Authentication', () => {
+  it('should not login without initialization', async () => {
+    const auth = AinizeAuth.getInstance();
+
+    await expect(auth.login()).rejects.toThrow(
+      'Authentication not initialized. Please call ainizeAuth.init() first.'
+    );
+  });
+
+  it('should login after initialization', async () => {
+    // NOTE(jiyoung): when initialize ainft, also initialize ainizeAuth.
+    const ainft = new AinftJs(process.env['PRIVATE_KEY']!, {
+      ainftServerEndpoint: 'https://ainft-api-dev.ainetwork.ai',
+      ainBlockchainEndpoint: 'https://testnet-api.ainetwork.ai',
+      chainId: 0,
+    });
+    const auth = AinizeAuth.getInstance();
+    await auth.login();
+
+    expect(auth.isLoggedIn()).toBe(true);
+  });
+
+  it('should logout after initialization', async () => {
+    const auth = AinizeAuth.getInstance();
+    await auth.logout();
+
+    expect(auth.isLoggedIn()).toBe(false);
+  });
+});
