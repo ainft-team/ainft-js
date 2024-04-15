0dc9a561a79eeceb85a7c4ff76e5673e0839e019
jiyoung <jiyoungan@comcom.ai>
2024-04-15 17:36:51 +0900

    feat: split request method into authenticated and basic version.

--- src/ainize.ts
+++ src/ainize.ts
@@ -5,7 +5,7 @@ import Service from '@ainize-team/ainize-js/dist/service';
 import { AIN_BLOCKCHAIN_CHAIN_ID } from './constants';
 import { getEnv } from './utils/env';
 
-const DEFAULT_TIMEOUT_MS = 60000;
+const DEFAULT_TIMEOUT_MS = 60000; // 1min
 
 export class AinizeService {
   private static instance: AinizeService;
@@ -34,8 +34,7 @@ export class AinizeService {
   }
 
   getServerName(): string {
-    // return 'openai_backend';
-    return 'ainize_openai'; // 이름 바꿔주세요!
+    return 'ainize_openai'; // TODO(jiyoung): change name.
   }
 
   async login(ain: Ain) {
@@ -63,14 +62,38 @@ export class AinizeService {
     );
     const server = await this.getServer(serverName);
     try {
-      await this.login(ain);
-      const resp = await Promise.race([server.request({ ...data, jobType: opType }), startTimer]);
-      if (resp.status === AinizeStatus.FAIL) {
-        throw new Error(JSON.stringify(resp.data));
+      const response = await Promise.race([server.request({ ...data, jobType: opType }), startTimer]);
+      if (response.status === AinizeStatus.FAIL) {
+        throw new Error(JSON.stringify(response.data));
       }
-      return resp as AinizeResponse<T>;
+      return response as AinizeResponse<T>;
     } catch (error: any) {
-      throw new Error(`failed to request ${opType}: ${error.message}`);
+      throw new Error(`Failed to request ${opType}: ${error.message}`);
+    } finally {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    }
+  }
+
+  async requestWithAuth<T>(
+    ain: Ain,
+    { serverName, opType, data, timeout = DEFAULT_TIMEOUT_MS }: AinizeRequest
+  ): Promise<AinizeResponse<T>> {
+    let timer;
+    const startTimer = new Promise(
+      (reject) => (timer = setTimeout(() => reject(`timeout of ${timeout}ms exceeded`), timeout))
+    );
+    const server = await this.getServer(serverName);
+    try {
+      await this.login(ain);
+      const response = await Promise.race([server.request({ ...data, jobType: opType }), startTimer]);
+      if (response.status === AinizeStatus.FAIL) {
+        throw new Error(JSON.stringify(response.data));
+      }
+      return response as AinizeResponse<T>;
+    } catch (error: any) {
+      throw new Error(`Failed to request ${opType}: ${error.message}`);
     } finally {
       if (timer) {
         clearTimeout(timer);
@@ -100,27 +123,28 @@ export enum AinizeStatus {
 export enum OperationType {
   CREATE_ASSISTANT = 'create_assistant',
   LIST_ASSISTANTS = 'list_assistants',
-  GET_ASSISTANT = 'retrieve_assistant',
-  UPDATE_ASSISTANT = 'modify_assistant',
+  RETRIEVE_ASSISTANT = 'retrieve_assistant',
+  MODIFY_ASSISTANT = 'modify_assistant',
   DELETE_ASSISTANT = 'delete_assistant',
   MINT_CREATE_ASSISTANT = 'mint_create_assistant',
 
   CREATE_THREAD = 'create_thread',
   LIST_THREADS = 'list_threads',
-  GET_THREAD = 'retrieve_thread',
-  UPDATE_THREAD = 'modify_thread',
+  RETRIEVE_THREAD = 'retrieve_thread',
+  MODIFY_THREAD = 'modify_thread',
   DELETE_THREAD = 'delete_thread',
+  CREATE_RUN_THREAD = 'create_run_thread',
 
   CREATE_MESSAGE = 'create_message',
   LIST_MESSAGES = 'list_messages',
-  GET_MESSAGE = 'retrieve_message',
-  UPDATE_MESSAGE = 'modify_message',
+  RETRIEVE_MESSAGE = 'retrieve_message',
+  MODIFY_MESSAGE = 'modify_message',
 
   CREATE_RUN = 'create_run',
   LIST_RUNS = 'list_runs',
   LIST_RUN_STEPS = 'list_run_steps',
-  GET_RUN = 'retrieve_run',
-  GET_RUN_STEP = 'retrieve_run_step',
-  UPDATE_RUN = 'modify_run',
+  RETRIEVE_RUN = 'retrieve_run',
+  RETRIEVE_RUN_STEP = 'retrieve_run_step',
+  MODIFY_RUN = 'modify_run',
   CANCEL_RUN = 'cancel_run',
 }
