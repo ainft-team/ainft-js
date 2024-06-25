ed203a122859cf23673edaeee33aa115fa2b0b96
Yoojin Ko <db232100@gmail.com>
2024-06-25 16:20:51 +0900

    fix: setProvider

--- src/ainft.ts
+++ src/ainft.ts
@@ -112,7 +112,7 @@ export default class AinftJs {
    * @param chainId
    */
   setAiNetworkInfo(providerUrl: string, chainId: number, axiosConfig?: any) {
-    this.ain.setProvider(providerUrl, undefined, chainId, axiosConfig);
+    this.ain.setProvider(providerUrl, null, chainId, axiosConfig);
   }
 
   /**
